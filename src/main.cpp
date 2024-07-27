16654361b717a1c911accd0e86d5c0c7e4ce5ac8
lixx299792458 <15726089100@qq.com>
2024-07-27 11:42:33 +0800

    对OLED显示进行了初步测试

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,31 @@
 #include <Arduino.h>
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <SPI.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//GPIO解锁
+//esp32-c3简直就是在诈骗
+// ESP32C3的GPIO11(VDD_SPI)默认功能是给flash供电，本开发板的Flash的VDD直接接3.3，所以可以将此IO用作GPIO.
+// 以下是操作流程，注意以下的操作只能执行一次，更改后不能复原（因为是设置熔丝位，不是寄存器，一次性操作）
+// 使用python的pip安装esptool。pip install esptool
+// 将开发板插入电脑, 在设备管理器中可以看到端口, 记录端口号, 例如 COM20
+// 打开命令行窗口输入espefuse -p COM20 burn_efuse VDD_SPI_AS_GPIO 1
+// 看提示，输入’BURN’
+//屏幕相关定义
+//U8G2_SSD1306_128X64_NONAME_1_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
+U8G2_SSD1306_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R0, 11, 7, 6);
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
   pinMode(12,OUTPUT);
   pinMode(13,OUTPUT);
-
+	u8g2.begin();
+	u8g2.firstPage();
+	do {
+		u8g2.setFont(u8g2_font_VCR_OSD_tu);
+		u8g2.setCursor(1, 20);
+		u8g2.print("hello,esp32-c3");
+	} while ( u8g2.nextPage() );
 }
 
 void loop() {
@@ -20,9 +37,4 @@ void loop() {
   digitalWrite(13,LOW);
   delay(1000);
 
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }

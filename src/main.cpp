3024edb0d5d3051d5f944fae8762183d036c6507
lixx299792458 <15726089100@qq.com>
2024-07-27 23:21:45 +0800

    初步形成6个菜单

--- src/main.cpp
+++ src/main.cpp
@@ -25,9 +25,43 @@ U8G2_SSD1306_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R0, 11, 7, 6);
 // #define CLK 4 // CLK ENCODER 
 // #define DT 5 // DT ENCODER 
 // ESP32Encoder encoder;
-Encoder myEnc(4, 5);
+Encoder myEnc(5, 4);
 long oldPosition  = -999;
 
+//定义整个GUI需要的全局变量
+//6个菜单
+uint8_t item_index = 0;
+int32_t newPosition = 0;
+//1、漆包线线径，单位mm，精度0.01
+char itemA[] = "wire dia:";
+int32_t wire_diameter_init = 0;
+int32_t wire_diameter_encoder = 0;
+int32_t wire_diameter = 0;
+//2、单层匝数
+char itemB[] = "layer turns:";
+int32_t layer_turns_init = 0;
+int32_t layer_turns_encoder = 0;
+int32_t layer_turns = 0;
+//3、总匝数
+char itemC[] = "total truns:";
+int32_t total_turns_init = 0;
+int32_t total_turns_encoder = 0;
+int32_t total_turns = 0;
+//4、回程误差设置
+char itemD[] = "return error:";
+int32_t return_error_init = 0;
+int32_t return_error_encoder = 0;
+int32_t return_error = 0;
+//5、对刀步距
+char itemE[] = "setting step:";
+int32_t setting_step_init = 0;
+int32_t setting_step_encoder = 0;
+int32_t setting_step = 0;
+//6、移动对刀
+char itemF[] = "move steps:";
+int32_t move_steps_init = 0;
+int32_t move_steps_encoder = 0;
+int32_t move_steps = 0;
 
 void setup() {
 	// //板载LED测试部分
@@ -42,17 +76,25 @@ void setup() {
 	//屏幕初始化
 	
 	u8g2.begin();
-	u8g2.firstPage();
-	do {
-		u8g2.setFont(u8g2_font_VCR_OSD_tu);
-		u8g2.setFontMode(1);
-		u8g2.setDrawColor(1);
-		u8g2.drawBox(0, 0, 127, 63);
-		u8g2.setDrawColor(2);
-		u8g2.setCursor(1, 20);
-		u8g2.print("1234");
-	} while ( u8g2.nextPage() );
+	// u8g2.firstPage();
+	// // do {
+	// // 	u8g2.setFont(u8g2_font_VCR_OSD_tu);
+	// // 	u8g2.setFontMode(1);
+	// // 	u8g2.setDrawColor(1);
+	// // 	u8g2.drawBox(0, 0, 127, 63);
+	// // 	u8g2.setDrawColor(2);
+	// // 	u8g2.setCursor(1, 20);
+	// // 	u8g2.print("1234");
+	// // } while ( u8g2.nextPage() );
+	// do {
+	// 	u8g2.setFont(u8g2_font_VCR_OSD_tu);
+	// 	u8g2.setCursor(1, 20);
+	// 	u8g2.print("1234");
 
+	// 	u8g2.setDrawColor(2);
+	// 	u8g2.drawBox(0, 0, 127, 24);
+
+	// } while ( u8g2.nextPage() );
 	#ifdef DEBUG
 		Serial.begin(9600);
 		// Serial.println("Basic Encoder Test:");
@@ -63,12 +105,12 @@ void setup() {
 
 void loop() {
 	//板载LED测试部分
-	digitalWrite(12,HIGH);
-	// digitalWrite(13,HIGH);
-	delay(1000);
-	digitalWrite(12,LOW);
-	// digitalWrite(13,LOW);
-	delay(1000);
+	// digitalWrite(12,HIGH);
+	// // digitalWrite(13,HIGH);
+	// delay(1000);
+	// digitalWrite(12,LOW);
+	// // digitalWrite(13,LOW);
+	// delay(1000);
 	//蜂鸣器测试部分
 	// digitalWrite(19,HIGH);
 	// delay(1000);
@@ -80,13 +122,187 @@ void loop() {
 	//     oldPosition = newPosition;
 	//     Serial.println(newPosition);
 	//   }
+	//处理菜单循环按键
 	if (0 == digitalRead(8)){
 		//消抖
 		delay(20);
 		while(!digitalRead(8));
 		delay(20);
 		// Serial.print("key1 click");
+		//自增并对6取余数，完成菜单位置切换
+		item_index ++;
+		item_index = item_index%6;
+		//根据状态直接来一波处理，恢复编码器变量
+		//主要原因就是这个操作只需要执行一次，因此放在此处最完美
+		switch (item_index)
+		{
+			case 0:
+				myEnc.write(wire_diameter_encoder);
+				break;
+			case 1:
+				myEnc.write(layer_turns_encoder);
+				break;			
+			case 2:
+				myEnc.write(total_turns_encoder);
+				break;
+			case 3:
+				myEnc.write(return_error_encoder);
+				break;				
+			case 4:
+				myEnc.write(setting_step_encoder);
+				break;
+			case 5:
+				myEnc.write(move_steps_encoder);
+				break;								
+			default:
+				break;
+		}
 	}
+	//根据菜单位置处理显示及其他问题
+	switch (item_index) {
+		case 0:
+			//首先显示前三行并选中第一行
+			u8g2.firstPage();
+			do {
+				u8g2.setFont(u8g2_font_7x14B_tf);
+				u8g2.setCursor(3, 16);
+				u8g2.print(itemA);u8g2.print(wire_diameter);//u8g2.print("um");
+				u8g2.setCursor(3, 37);
+				u8g2.print(itemB);u8g2.print(layer_turns);
+				u8g2.setCursor(3, 58);
+				u8g2.print(itemC);u8g2.print(total_turns);
+				u8g2.setDrawColor(2);
+				u8g2.drawBox(1, 1, 127, 20);
+			} while ( u8g2.nextPage() );
+			break;
+		case 1:
+			//首先显示前三行并选中第2行
+			u8g2.firstPage();
+			do {
+				u8g2.setFont(u8g2_font_7x14B_tf);
+				u8g2.setCursor(3, 16);
+				u8g2.print(itemA);u8g2.print(wire_diameter);
+				u8g2.setCursor(3, 37);
+				u8g2.print(itemB);u8g2.print(layer_turns);
+				u8g2.setCursor(3, 58);
+				u8g2.print(itemC);u8g2.print(total_turns);
+				u8g2.setDrawColor(2);
+				u8g2.drawBox(1, 22, 127, 41);
+			} while ( u8g2.nextPage() );
+			break;
+		case 2:
+			//首先显示前三行并选中第3行
+			u8g2.firstPage();
+			do {
+				u8g2.setFont(u8g2_font_7x14B_tf);
+				u8g2.setCursor(3, 16);
+				u8g2.print(itemA);u8g2.print(wire_diameter);
+				u8g2.setCursor(3, 37);
+				u8g2.print(itemB);u8g2.print(layer_turns);
+				u8g2.setCursor(3, 58);
+				u8g2.print(itemC);u8g2.print(total_turns);
+				u8g2.setDrawColor(2);
+				u8g2.drawBox(1, 43, 127, 62);
+			} while ( u8g2.nextPage() );
+			break;
+		case 3:
+			//首先显示后三行并选中第1行
+			u8g2.firstPage();
+			do {
+				u8g2.setFont(u8g2_font_7x14B_tf);
+				u8g2.setCursor(3, 16);
+				u8g2.print(itemD);u8g2.print(return_error);
+				u8g2.setCursor(3, 37);
+				u8g2.print(itemE);u8g2.print(setting_step);
+				u8g2.setCursor(3, 58);
+				u8g2.print(itemF);u8g2.print(move_steps);
+				u8g2.setDrawColor(2);
+				u8g2.drawBox(1, 1, 127, 20);
+			} while ( u8g2.nextPage() );
+			break;
+		case 4:
+			//首先显示后三行并选中第2行
+			u8g2.firstPage();
+			do {
+				u8g2.setFont(u8g2_font_7x14B_tf);
+				u8g2.setCursor(3, 16);
+				u8g2.print(itemD);u8g2.print(return_error);
+				u8g2.setCursor(3, 37);
+				u8g2.print(itemE);u8g2.print(setting_step);
+				u8g2.setCursor(3, 58);
+				u8g2.print(itemF);u8g2.print(move_steps);
+				u8g2.setDrawColor(2);
+				u8g2.drawBox(1, 22, 127, 41);
+			} while ( u8g2.nextPage() );
+			break;
+		case 5:
+			//首先显示后三行并选中第2行
+			u8g2.firstPage();
+			do {
+				u8g2.setFont(u8g2_font_7x14B_tf);
+				u8g2.setCursor(3, 16);
+				u8g2.print(itemD);u8g2.print(return_error);
+				u8g2.setCursor(3, 37);
+				u8g2.print(itemE);u8g2.print(setting_step);
+				u8g2.setCursor(3, 58);
+				u8g2.print(itemF);u8g2.print(move_steps);
+				u8g2.setDrawColor(2);
+				u8g2.drawBox(1, 43, 127, 62);
+			} while ( u8g2.nextPage() );
+			break;
+		default:
+			// code block
+			break;
+	}
+	//处理编码器的问题
+	switch (item_index)	{
+		case 0:
+			newPosition = myEnc.read();
+			if (newPosition != wire_diameter_encoder) {
+				wire_diameter_encoder = newPosition;
+			}
+			wire_diameter = (wire_diameter_init + wire_diameter_encoder/4)*10;
+			break;
+		case 1:
+			newPosition = myEnc.read();
+			if (newPosition != layer_turns_encoder) {
+				layer_turns_encoder = newPosition;
+			}
+			layer_turns = layer_turns_init + layer_turns_encoder/4;
+			break;			
+		case 2:
+			newPosition = myEnc.read();
+			if (newPosition != total_turns_encoder) {
+				total_turns_encoder = newPosition;
+			}
+			total_turns = total_turns_init + total_turns_encoder/4;
+			break;	
+		case 3:
+			newPosition = myEnc.read();
+			if (newPosition != return_error_encoder) {
+				return_error_encoder = newPosition;
+			}
+			return_error = return_error_init + return_error_encoder/4;
+			break;			
+		case 4:
+			newPosition = myEnc.read();
+			if (newPosition != setting_step_encoder) {
+				setting_step_encoder = newPosition;
+			}
+			setting_step = setting_step_init + setting_step_encoder/4;
+			break;	
+		case 5:
+			newPosition = myEnc.read();
+			if (newPosition != move_steps_encoder) {
+				move_steps_encoder = newPosition;
+			}
+			move_steps = move_steps_init + move_steps_encoder/4;
+			//此处还要驱动步进电机定位运动
+			break;								
+		default:
+			break;
+	}
+	//切换运行状态
 	if (0 == digitalRead(13)){
 		//消抖
 		delay(20);
